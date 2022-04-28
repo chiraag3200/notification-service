124224eb8fe270f7f3b3f40ba52ea8c1c43b0aec
chiraag3200 <chiraagmittal05@gmail.com>
2022-04-28 19:57:39 +0530

    Database connection established

--- server.js
+++ server.js
@@ -10,6 +10,14 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
+const uri = process.env.ATLAS_URI
+mongoose.connect(uri)
+
+const connection = mongoose.connection
+connection.once('open', () => {
+  console.log('Mongoose DB connection established successfully')
+})
+
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`)
 })

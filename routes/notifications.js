8e0de7a6155201cf8a10fb8bd23e06c1bd425712
chiraag3200 <chiraagmittal05@gmail.com>
2022-04-29 15:58:56 +0530

    different meduims functionality integrated

--- routes/notifications.js
+++ routes/notifications.js
@@ -2,19 +2,32 @@ const router = require('express').Router();
 let User = require('../models/users.models');
 const { sendEmail } = require("../utils/send_email.js");
 
+
+// route to send notifications using different mediums ( can be used for ad hoc as well as scheduled notofications )
 router.route('/send').post((req, res) => {
 
-
+  var meduim = req.headers['meduim'];
   User.find({} , (err, users) => {
       if(err){
         res.status(400).json('Error: ' + err);
       }
 
+      // different mediums can be integrated here
       users.map(user => {
         try {
-          sendEmail(user.email, req.body.subject, req.body.body);
+
+          // email notifications
+          if (meduim === 'email'){
+            sendEmail(user.email, req.body.subject, req.body.body);
+          }
+          if (meduim === 'sms'){
+            // integrate send SMS function
+          }
+          if (meduim === 'slack'){
+            // integrate send slack message function
+          }
           return res.status(200).send({
-            message: "Email sent successfully"
+            message: `${meduim} sent successfully`   
           });
         }
         catch (err){
--- routes/user.js
+++ routes/user.js
@@ -1,16 +1,20 @@
 const router = require('express').Router();
 let User = require('../models/users.models');
 
+// a route to get all the users
 router.route('/').get((req, res) => {
   User.find()
     .then(users => res.json(users))
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
+
+// a route to add a new user
 router.route('/register').post((req, res) => {
 
   const {email} = req.body
 
+  // check if user already exists
   User.findOne({email:email})
   .then(user=>{
     if(user){
@@ -20,6 +24,7 @@ router.route('/register').post((req, res) => {
     }
   })
 
+  // create a new user
   const newUser = new User(req.body);
   newUser.save()
     .then(() => res.json('User added!'))
